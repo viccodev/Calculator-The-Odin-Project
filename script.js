ab4ed1e6003a18d0c46003d21fb8cc28f628214b
viccodev <osoriovictor.dev@gmail.com>
2026-05-18 16:09:32 -0400

    Doing Structure in HTML and adding CSS, flexbox for centering and styling basic things

--- script.js
+++ script.js
@@ -33,6 +33,32 @@ let operation = [];
 // Making Site
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* OLD CODE
+
 // General options to avoid rare behavior
 // document.body.style.backgroundColor = "gray";
 document.body.style.width = "100%";
@@ -41,29 +67,20 @@ document.body.style.boxSizing = "border-box";
 document.body.style.padding = "0px";
 document.body.style.margin = "0px";
 
-/* to copy 
 
-    <div class="getPremiumAd">
-        <p>Get the premium calculator for only 9999999.99 bucks! <a href="https://youtu.be/dQw4w9WgXcQ">HERE!</a> </p>
-    </div>
-    <div id="calculatorContainer">
-        <div id="calculator">
-            <div class="lastOperationBar"></div>
-            <div class="calcDisplay"></div>
-            <div class="calcButtonsContainer"></div>
-            
-        </div>
-    </div>
-*/
 
 // TOP get premium bar as joke.
-
-let getPremium = document.createElement("div");
 let mainContainer = document.querySelector("#mainContainer");
-document.body.appendChild(getPremium);
+let getPremium = document.createElement("div");
+mainContainer.style.display = "flex";
+mainContainer.style.flexDirection = "column";
+mainContainer.style.alignItems = "center";
+mainContainer.appendChild(getPremium);
 
 let getPremiumText = document.createElement("p");
 getPremiumText.textContent = "Get the premium calculator for only 9999999.99 bucks!";
+getPremiumText.style.borderRadius = "5%";
+getPremiumText.style.backgroundColor = "gray";
 
 let getPremiumLink = document.createElement("a");
 getPremiumLink.href = "https://youtu.be/dQw4w9WgXcQ"
@@ -74,4 +91,30 @@ getPremium.appendChild(getPremiumText);
 
 // Calculator start
 
+/* to copy 
 
+    <div class="getPremiumAd">
+        <p>Get the premium calculator for only 9999999.99 bucks! <a href="https://youtu.be/dQw4w9WgXcQ">HERE!</a> </p>
+    </div>
+
+    
+    <div id="calculatorContainer">
+        <div id="calculator">
+            <div class="lastOperationBar"></div>
+            <div class="calcDisplay"></div>
+            <div class="calcButtonsContainer"></div>
+            
+        </div>
+    </div>
+
+
+
+let calculatorContainer = document.createElement("div");
+calculatorContainer.style.display = "flex";
+calculatorContainer.id = "calculatorContainer";
+calculatorContainer.style.width = "350px";
+calculatorContainer.style.height = "500px";
+calculatorContainer.style.outline = "1px solid black";
+calculatorContainer.style.borderRadius = "2%";
+mainContainer.appendChild(calculatorContainer);
+*/
